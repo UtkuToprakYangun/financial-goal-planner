2174302ddeb61df5e656bfd3afb0c46e5af16c19
UtkuToprakYangun <toprakyangunnn@gmail.com>
2026-02-07 01:55:56 +0300

    Add initial watch and salary data

---
+++ main.py
@@ -0,0 +1,28 @@
+print("""PLEASE WRITE DOWN THE NUMBER NEXT TO THE WATCH YOU WANT.
+      1-Rolex Datejust 41(16.500$)
+      2-Rolex Yacht-Master II(22.500$)
+      3-Patek Philippe Aquanaut 5167A(70.000$)
+      4-Cartier Santos de Cartier(8.000$)
+      5-Audemars Piguet Royal Oak 15510ST(50.000$)
+      """)
+
+WATCH_PRICES_USD = {
+    1 : 16500,
+    2 : 22500,
+    3 : 70000,
+    4 : 8000,
+    5 : 50000
+}
+
+SALARY_DATA_USD = {
+    "turkey" : {
+        "junior" : 1000,
+        "mid"    : 1500,
+        "senior" : 2000
+                }, 
+    "abroad" : {
+        "junior" : 3000,
+        "mid"    : 5000,
+        "senior" : 7000
+     }
+}
